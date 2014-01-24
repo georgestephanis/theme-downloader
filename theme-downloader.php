bb8584add9cf0039580902632e30652829798f52
George Stephanis <george@stephanis.info>
2014-01-24 11:23:02 -0500

    Add new version that supports the 3.8 THX38
    But it needs an action or two added to core first.  The diff for that
    is attached as well.

--- theme-downloader.php
+++ theme-downloader.php
@@ -18,7 +18,12 @@ class Theme_Downloader_Plugin {
 
 		add_filter( 'theme_action_links', array( $this, 'theme_action_links' ), 10, 3 );
 		add_action( 'wp_ajax_download_theme', array( $this, 'wp_ajax_download_theme' ) );
-		add_action( 'admin_footer-themes.php', array( $this, 'admin_footer_themes_php' ) );
+		if ( version_compare( $GLOBALS['wp_version'], '3.8', '<' ) ) {
+			add_action( 'admin_footer-themes.php', array( $this, 'admin_footer_themes_php' ) );
+		} else {
+			add_filter( 'wp_prepare_themes_for_js', array( $this, 'wp_prepare_themes_for_js' ) );
+			add_action( 'tmpl-theme-single_actions', array( $this, 'tmpl_theme_single_actions' ) );
+		}
 	}
 
 	function theme_action_links( $actions, $theme, $ms_theme = null ) {
@@ -68,6 +73,29 @@ class Theme_Downloader_Plugin {
 		$download_url = esc_url( add_query_arg( $args, admin_url( 'admin-ajax.php' ) ) );
 		?>
 		<script>jQuery('#current-theme .theme-options ul').prepend('<li><a href="<?php echo $download_url; ?>"><?php echo esc_attr__('Download'); ?></a></li>');</script>
+		<?php
+	}
+
+	function wp_prepare_themes_for_js( $prepared_themes ) {
+		foreach ( $prepared_themes as $slug => $data ) {
+			$download_url = null;
+			if ( current_user_can( 'edit_themes' ) && Theme_Downloader::can_zip() ) {
+				$args = array(
+					'action' => 'download_theme',
+					'theme' => $slug,
+				);
+				$download_url = esc_url( add_query_arg( $args, admin_url( 'admin-ajax.php' ) ) );
+			}
+			$prepared_themes[ $slug ]['actions']['download'] = $download_url;
+		}
+		return $prepared_themes;
+	}
+
+	function tmpl_theme_single_actions( $active_or_inactive ) {
+		?>
+		<# if ( data.actions.download ) { #>
+			<a href="{{{ data.actions.download }}}" target="_blank" class="button button-secondary" title="<?php esc_attr_e( 'Download' ); ?>"><?php esc_html_e( 'Download' ); ?></a>
+		<# } #>
 		<?php
 	}
 }
